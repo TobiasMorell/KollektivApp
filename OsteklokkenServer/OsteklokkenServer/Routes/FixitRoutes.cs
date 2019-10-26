908faaeaaa46831d4c5b5a9ddab21626784114ca
rosenbjerg <rosenbjerg@users.noreply.github.com>
2019-10-26 22:52:56 +0200

    Upgrade and refactor

---
+++ OsteklokkenServer/OsteklokkenServer/Routes/FixitRoutes.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using LiteDB;
+using Red.Interfaces;
+
+namespace OsteklokkenServer.Routes
+{
+    public static class FixitRoutes
+    {
+        public static void Register(IRouter router, LiteDatabase db)
+        {
+            var fixits = db.GetCollection<Fixit>("fixits");
+            
+            router.Get("", Utils.Authed, (req, res) =>
+            {
+                var items = fixits.FindAll();
+                return res.SendJson(items);
+            });
+            
+            router.Get("/:img", (req, res) 
+                => res.SendFile("./public/" + req.Context.ExtractUrlParameter("img")));
+
+            router.Post("", Utils.Authed, async (req, res) =>
+            {
+                var form = await req.GetFormDataAsync();
+                if (!Fixit.TryValidate(form, out var fixit))
+                {
+                    return await res.SendString("'title' or 'description' is missing", status: HttpStatusCode.BadRequest);
+                }
+
+                fixit.Id = Guid.NewGuid().ToString("N");
+                if (form.Files.Any())
+                {
+                    try
+                    {
+                        var imgName = form.Files.GetFile("image").FileName;
+                        var ext = Path.GetExtension(imgName);
+
+                        await req.SaveFiles("public", old => fixit.Id + ext);
+                        fixit.ImagePath = fixit.Id + ext;
+                    }
+                    catch (Exception e)
+                    {
+                        return await res.SendString("Filen kunne ikke gemmes, den er nok for stor",
+                            status: HttpStatusCode.BadRequest);
+                    }
+                }
+                else
+                {
+                    fixit.ImagePath = "default.png";
+                }
+
+                fixits.Insert(fixit);
+                return await res.SendJson(fixit);
+            });
+
+            router.Put("", Utils.Authed, async (req, res) =>
+            {
+                var form = await req.GetFormDataAsync();
+                if (!Fixit.TryValidate(form, out var fixit, true))
+                {
+                    return await res.SendString("'title', 'description' or 'id' is missing.", status: HttpStatusCode.BadRequest);
+                }
+
+                var f = fixits.FindOne(i => i.Id == fixit.Id);
+                if (f == null)
+                {
+                    return await res.SendString("Invalid item Id", status: HttpStatusCode.BadRequest);
+                }
+
+                if (form.Files.Any())
+                {
+                    var imgName = form.Files.GetFile("image").FileName;
+                    var ext = Path.GetExtension(imgName);
+
+                    File.Delete(Path.Combine("public", f.ImagePath));
+                    await req.SaveFiles("public", old => f.Id + ext);
+                    fixit.ImagePath = f.Id + ext;
+                }
+                else
+                    fixit.ImagePath = f.ImagePath;
+
+                if (form.ContainsKey("done") && bool.TryParse(form["done"], out var done))
+                    fixit.Done = done;
+                else
+                    fixit.Done = f.Done;
+
+                fixits.Update(fixit);
+                //Append a random query to the end of the image url to force refresh on frontend
+                fixit.ImagePath += "?" + DateTime.Now.Ticks;
+                return await res.SendJson(fixit);
+            });
+
+            router.Delete("", Utils.Authed, async (req, res) =>
+            {
+                var form = await req.GetFormDataAsync();
+                if (!form.ContainsKey("id"))
+                {
+                    return await res.SendString("'id' is missing", status: HttpStatusCode.BadRequest);
+                }
+
+                fixits.Delete(form["id"].ToString());
+                return await res.SendStatus(HttpStatusCode.OK);
+            });
+        }
+    }
+}
---
+++ OsteklokkenServer/OsteklokkenServer/Routes/KollexiconRoutes.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using LiteDB;
+using Newtonsoft.Json;
+using Red.Interfaces;
+
+namespace OsteklokkenServer.Routes
+{
+    public static class KollexiconRoutes
+    {
+        public static void ImportRules(LiteDatabase db)
+        {
+            var rules = db.GetCollection<KollexiconRule>("kollexicon");
+                        
+            var plainRules = File.ReadAllText("rules.json");
+            var parsedRules = JsonConvert.DeserializeObject<List<KollexiconRule>>(plainRules);
+            rules.Delete(r => true);
+            rules.InsertBulk(parsedRules);
+        }
+        
+        public static void Register(IRouter router, LiteDatabase db)
+        {
+            var rules = db.GetCollection<KollexiconRule>("kollexicon");
+
+            
+            router.Get("", Utils.Authed, async (request, response) =>
+            {
+                var r = rules.FindAll().OrderBy(x => x.Title);
+                return await response.SendJson(r);
+            });
+
+            router.Post("", Utils.Authed, async (req, res) =>
+            {
+                var form = await req.GetFormDataAsync();
+                if (!form.ContainsKey("title") || !form.ContainsKey("description"))
+                {
+                    return await res.SendString("'title' or 'description' missing.", status: HttpStatusCode.BadRequest);
+                }
+
+                if (rules.FindOne(r => r.Title == form["title"]) != null)
+                {
+                    return await res.SendString("Der findes allerede en regl med det navn", status: HttpStatusCode.BadRequest);
+                }
+
+                var rule = new KollexiconRule { Title = form["title"], Description = form["description"] };
+                rules.Insert(rule);
+
+                return await res.SendJson(rule);
+            });
+
+            router.Put("", Utils.Authed, async (req, res) =>
+            {
+                var form = await req.GetFormDataAsync();
+                if (!form.ContainsKey("title") || !form.ContainsKey("description") || !form.ContainsKey("id"))
+                {
+                    return await res.SendString("'id', 'title' or 'description' missing.", status: HttpStatusCode.BadRequest);
+                }
+
+                string id = form["id"];
+                var rule = rules.FindOne(r => r.Id.Equals(id));
+                if (rule == null)
+                {
+                    return await res.SendString("No rule for the given id", status: HttpStatusCode.BadRequest);
+                }
+
+                rule.Title = form["title"];
+                rule.Description = form["description"];
+
+                rules.Update(rule);
+                return await res.SendJson(rule);
+            });
+
+            router.Delete("", Utils.Authed, async (req, res) =>
+            {
+                var form = await req.GetFormDataAsync();
+                if (!form.ContainsKey("id"))
+                {
+                    return await res.SendString("'id' is missing", status: HttpStatusCode.BadRequest);
+                }
+
+                string id = form["id"];
+                var m = rules.FindOne(r => r.Id.Equals(id));
+                if (m == null)
+                {
+                    return await res.SendString("No rule for the given id", status: HttpStatusCode.NotFound);
+                }
+
+                rules.Delete(m.Id);
+                return await res.SendStatus(HttpStatusCode.OK);
+            });
+        }
+    }
+}
