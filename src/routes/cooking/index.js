ac40e323076bcbe21e3c4ee4f1fc4e8cbb433c05
Tobias Morell <tobias.morell@aivero.com>
2020-03-17 22:27:45 +0100

    Add cookingList div again after merge

--- src/routes/cooking/index.js
+++ src/routes/cooking/index.js
@@ -141,18 +141,20 @@ export default class Cooking extends Component {
 	render(props, state) {
 		return (
 			<div className={['appContainer', style.scollable].join(' ')}>
-				{state.schedule.map(m => {
-					if (m === state.upForDeletion){
-						let item =  <CookingCard className={style.delete} menu={m} />;
-						setTimeout(() => {
-							this.setState({ schedule: state.schedule.filter(i => i.Week !== m.Week), upForDeletion: undefined });
-						}, 510);
-						return item;
-					}
-					return (<CookingCard menu={m} openEditMenu={this.openEditMenu(m)} deleteItem={this.deleteItem(m)}
-						session={Backend.getSessionDetails()} onAttend={this.attend(m)} onCancel={this.cancelAttendance(m)}
-					        />);
-				})}
+				<div className={style.cookingList}>
+					{state.schedule.map(m => {
+						if (m === state.upForDeletion){
+							let item =  <CookingCard className={style.delete} menu={m} />;
+							setTimeout(() => {
+								this.setState({ schedule: state.schedule.filter(i => i.Week !== m.Week), upForDeletion: undefined });
+							}, 510);
+							return item;
+						}
+						return (<CookingCard menu={m} openEditMenu={this.openEditMenu(m)} deleteItem={this.deleteItem(m)}
+							session={Backend.getSessionDetails()} onAttend={this.attend(m)} onCancel={this.cancelAttendance(m)}
+						        />);
+					})}
+				</div>
 
 				<Dialog onAccept={this.confirmMenuDialog} onCancel={this.clearItemDialog} ref={addItemDlg => this.addItemDlg = addItemDlg} >
 					<Dialog.Header>{state.addNewItem ? 'Tilføj en madplan' : 'Rediger en madplan'}</Dialog.Header>
